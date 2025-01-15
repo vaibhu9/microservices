f1deac5c73ea0353d51d1b60c6aaedef2b90c852
Vaibhav Ramdas Shejol <158761509+vaibhu9@users.noreply.github.com>
2025-01-16 02:14:37 +0530

    added service-d i.e. eureka client

---
+++ service-d/src/main/java/com/amazingcode/in/example/ServiceDApplication.java
@@ -0,0 +1,15 @@
+package com.amazingcode.in.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class ServiceDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDApplication.class, args);
+	}
+
+}
---
+++ service-d/src/main/java/com/amazingcode/in/example/controller/ServiceDController.java
@@ -0,0 +1,16 @@
+package com.amazingcode.in.example.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/service-d")
+public class ServiceDController {
+
+	@GetMapping
+	public ResponseEntity<String> getMessage(){
+		return ResponseEntity.ok("Hello from Service-D");
+	}
+}
